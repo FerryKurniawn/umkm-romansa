e8c12e6e774dc7258f5b6bd73434f4fdb69d4f33
FerryKurniawn <ferrysavvy@gmail.com>
2025-10-23 07:36:31 +0700

    feat(index, main): remove google analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,37 +1,7 @@
-/* eslint-disable no-undef */
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-
-document.addEventListener("DOMContentLoaded", () => {
-  const cookieBanner = document.getElementById("cookie-banner");
-  const acceptCookiesBtn = document.getElementById("accept-cookies");
-
-  if (!localStorage.getItem("cookiesAccepted")) {
-    cookieBanner.style.display = "block"; // Tampilkan banner
-  }
-
-  acceptCookiesBtn.addEventListener("click", () => {
-    localStorage.setItem("cookiesAccepted", "true");
-    cookieBanner.style.display = "none";
-    loadGoogleAnalytics();
-  });
-});
-
-function loadGoogleAnalytics() {
-  const script = document.createElement("script");
-  script.src = "https://www.googletagmanager.com/gtag/js?id=G-V32YCZQL7G";
-  script.onload = () => {
-    window.dataLayer = window.dataLayer || [];
-    function gtag() {
-      dataLayer.push(arguments);
-    }
-    gtag("js", new Date());
-    gtag("config", "G-V32YCZQL7G");
-  };
-  document.head.appendChild(script);
-}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
